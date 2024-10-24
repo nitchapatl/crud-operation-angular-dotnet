909ec0fed204b1c0405ba5ecfbe315cf04cfabb8
nitchapatl <nitchapat.lho@gmail.com>
2024-10-24 10:29:33 +0700

    commit project

---
+++ crud-angular/src/app/components/employee-list/employee-list.component.ts
@@ -0,0 +1,48 @@
+import { Component, inject } from '@angular/core';
+import { IEmployee } from '../../interfaces/employee';
+import { HttpService } from '../../http.service';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { Router, RouterLink } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-employee-list',
+  standalone: true,
+  imports: [MatTableModule, MatButtonModule, RouterLink],
+  templateUrl: './employee-list.component.html',
+  styleUrl: './employee-list.component.css'
+})
+export class EmployeeListComponent {
+
+  router=inject(Router);
+  employeeList:IEmployee[]=[];
+  httpService=inject(HttpService);
+  toaster=inject(ToastrService);
+  displayedColumns: string[] = ['id', 'name', 'email', 'age', 'phone', 'salary','action'];
+  ngOnInit(){
+  this.getEmployeeFromServer();
+}
+  getEmployeeFromServer(){
+    this.httpService.getAllEmployee().subscribe((result)=>{
+  
+      this.employeeList=result;
+      console.log(this.employeeList);
+    });
+  }
+  add() {
+    this.router.navigateByUrl("/create-employee");
+  }
+  edit(id:number){
+    console.log(id);
+    this.router.navigateByUrl("/employee/"+id);
+  }
+  delete(id:number){
+    this.httpService.deleteEmployee(id).subscribe(()=>{
+      console.log("deleted");
+      
+      this.getEmployeeFromServer();
+      this.toaster.success("Record deleted successfully.");
+    });
+  }
+}
